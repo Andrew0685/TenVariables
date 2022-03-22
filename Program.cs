7aeff407aa74b89553686a49907bc4e9c0d66f73
atshibko <atshibko@gmail.com>
2022-03-22 11:10:40 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TenVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string employeeName;
+            string employeeDateOfBirth;
+            string employeeAddress;
+            string employeeEMail;
+            string employeePhoneNumber;
+            char employeeGender;
+            int employeeAge;
+            string employeeID;
+            bool employeeMaritalStatus;
+            decimal employeeAnnualIncome;
+        }
+    }
+}
